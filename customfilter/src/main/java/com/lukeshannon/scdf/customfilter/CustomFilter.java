2ee51f70ac8d99a7c1adbb479393d3981ffe0c98
lshannon <luke.shannon@gmail.com>
2018-06-13 18:20:10 -0400

    First check in

---
+++ customfilter/src/main/java/com/lukeshannon/scdf/customfilter/CustomFilter.java
@@ -0,0 +1,5 @@
+package com.lukeshannon.scdf.customfilter;
+
+public class CustomFilter {
+
+}
---
+++ customfilter/src/main/java/com/lukeshannon/scdf/customfilter/CustomfilterApplication.java
@@ -0,0 +1,12 @@
+package com.lukeshannon.scdf.customfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomfilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomfilterApplication.class, args);
+	}
+}
